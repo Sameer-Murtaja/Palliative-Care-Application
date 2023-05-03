d490ec924a36ab636e2c27f031b1329eb6ca8f3e
Sameer <SameerMMurtaja@gmail.com>
2023-05-03 05:14:54 +0300

    add topics recycler view with adapter and show topics from firebase

--- app/src/main/java/com/example/palliativecareapplication/MainScreenFragment.kt
+++ app/src/main/java/com/example/palliativecareapplication/MainScreenFragment.kt
@@ -1,59 +1,116 @@
 package com.example.palliativecareapplication
 
+import android.app.ProgressDialog
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.palliativecareapplication.adapter.TopicAdapter
+import com.example.palliativecareapplication.databinding.FragmentMainScreenBinding
+import com.example.palliativecareapplication.model.FirebaseNames
+import com.example.palliativecareapplication.model.Topic
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MainScreenFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MainScreenFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    lateinit var db: FirebaseFirestore
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    lateinit var binding: FragmentMainScreenBinding
+    private var progressDialog: ProgressDialog? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_screen, container, false)
+        binding = FragmentMainScreenBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MainScreenFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MainScreenFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        db = Firebase.firestore
     }
+
+
+    override fun onResume() {
+        super.onResume()
+        showDialog("Loading topics");
+        val topicsArr = ArrayList<Topic>()
+        db.collection(FirebaseNames.COLLECTION_TOPICS)
+            .get()
+            .addOnSuccessListener { querySnapshot ->
+                for (document in querySnapshot) {
+                    val id = document.id
+                    val title = document.getString("title")!!
+                    val description = document.getString("description")!!
+                    val doctorName = document.getString("doctorName")!!
+                    val image = document.getString("image")!!
+                    topicsArr.add(
+                        Topic(
+                            id,
+                            title,
+                            description,
+                            doctorName,
+                            image
+                        )
+                    )
+                }
+                val productsAdapter = TopicAdapter(topicsArr)
+                binding.rvTopics.layoutManager = LinearLayoutManager(requireContext())
+                binding.rvTopics.adapter = productsAdapter
+                hideDialog()
+            }.addOnFailureListener { error ->
+                Log.e("hzm", error.message.toString())
+                hideDialog()
+                Toast.makeText(requireContext(), "Error while retrieving data", Toast.LENGTH_SHORT).show()
+            }
+
+
+
+        binding.btnAdd.setOnClickListener {
+            MainActivity.swipeFragment(requireActivity(), AddTopicFragment())
+        }
+
+
+        binding.textInputSearch.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                (binding.rvTopics.adapter as TopicAdapter).search(s.toString())
+            }
+
+        })
+
+    }
+
+
+    private fun showDialog(msg: String) {
+        progressDialog = ProgressDialog(requireContext())
+        progressDialog!!.setMessage(msg)
+        progressDialog!!.setCancelable(false)
+        progressDialog!!.show()
+    }
+
+
+    private fun hideDialog() {
+        if (progressDialog!!.isShowing)
+            progressDialog!!.dismiss()
+    }
+
 }
