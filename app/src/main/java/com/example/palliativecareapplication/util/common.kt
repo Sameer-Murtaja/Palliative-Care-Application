ee2188efca68c93d7bba9120833ff80bf179d56c
Bilal Alkhatib <bilaloalkhatib.02@gmail.com>
2023-05-20 18:40:34 +0300

    add common kotlin file to make it contain all common kotlin code

--- app/src/main/java/com/example/palliativecareapplication/util/common.kt
+++ app/src/main/java/com/example/palliativecareapplication/util/common.kt
@@ -3,14 +3,14 @@ package com.example.palliativecareapplication.util
 import androidx.fragment.app.Fragment
 import com.example.palliativecareapplication.R
 
-private fun Fragment.navigateWithReplaceFragment(fragment: Fragment) {
+ fun Fragment.navigateWithReplaceFragment(fragment: Fragment) {
     requireActivity().supportFragmentManager.beginTransaction().apply {
         replace(R.id.fragmentContainerView, fragment)
         commit()
     }
 }
 
-private fun Fragment.navigateWithAddFragment(fragment: Fragment) {
+ fun Fragment.navigateWithAddFragment(fragment: Fragment) {
     requireActivity().supportFragmentManager.beginTransaction().apply {
         add(R.id.fragmentContainerView, fragment)
         addToBackStack(fragment::class.java.name)
